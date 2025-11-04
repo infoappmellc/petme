3c9887c2a52aa0d9d0d8a1d93737ba2f3e38a3ac
infoappmellc <info.appme.llc@gmail.com>
2025-11-04 14:50:18 +0700

    fix: set runtime='edge' and update Cloudflare config

---
+++ frontend/open-next.config.ts
@@ -0,0 +1,8 @@
+import { defineCloudflareConfig } from '@opennextjs/cloudflare/config';
+import r2IncrementalCache from '@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache';
+
+export default defineCloudflareConfig({
+  overrides: {
+    incrementalCacheHandler: r2IncrementalCache,
+  },
+});
