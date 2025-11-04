1e47d3146d4b22b5899b38b9155da476fee10717
infoappmellc <info.appme.llc@gmail.com>
2025-11-04 15:31:26 +0700

    chore: configure OpenNext for Cloudflare

--- frontend/open-next.config.ts
+++ frontend/open-next.config.ts
@@ -2,7 +2,5 @@ import { defineCloudflareConfig } from '@opennextjs/cloudflare/config';
 import r2IncrementalCache from '@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache';
 
 export default defineCloudflareConfig({
-  overrides: {
-    incrementalCacheHandler: r2IncrementalCache,
-  },
+  incrementalCache: r2IncrementalCache,
 });
